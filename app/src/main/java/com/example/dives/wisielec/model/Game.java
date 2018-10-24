fc301e736684f1164cb06fc8690d400007c24bb6
pPaszynski <dives2112@gmail.com>
2018-10-24 23:41:22 +0200

    Add new words

--- app/src/main/java/com/example/dives/wisielec/model/Game.java
+++ app/src/main/java/com/example/dives/wisielec/model/Game.java
@@ -7,7 +7,15 @@ import java.util.Random;
 
 public class Game {
 
-    private List<String> countries = Arrays.asList("Polska", "USA", "Japonia", "Portugalia");
+    //private List<String> countries = Arrays.asList("Polska", "USA", "Japonia", "Portugalia", "Albania", );
+    private List<String> countries = Arrays.asList("Albania", "Andorra", "Armenia", "Austria",
+            "Azerbaijan", "Belarus", "Belgium", "Bosnia", "Bulgaria", "Croatia",
+            "Cyprus", "Czech", "Denmark", "Estonia", "Finland", "France", "Georgia",
+            "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia",
+            "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Malta", "Moldova", "Monaco",
+            "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia",
+            "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland",
+            "Turkey", "Ukraine", "Vatican");
     private List<String> cities = Arrays.asList("Warszawa", "Krakow", "Dupa", "pipppa");
     private String category;
     private String word;
